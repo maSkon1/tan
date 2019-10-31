4d0865cb6cf39db173b1773db04e993f1aef8bd3
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 22:20:34 +0400

    ver 0.3

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-const os = require("os");
-const greeting = require("./greeting");
-// получим имя текущего пользователя
-let userName = os.userInfo().username;
-console.log(`Дата запроса: ${greeting.date}`);
-console.log(greeting.getMessage(userName));
+'use strict';
+const User = require("./user.js");
+let petr = new User("Petr", 25);
+petr.sayHi();
