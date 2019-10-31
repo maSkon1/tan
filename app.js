ce498122f9ec8b0331164b1775b7ab931dda1f6e
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:51:49 +0400

    ver 0.13.2

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 const fs = require("fs");
+const zlib = require("zlib");
 let readableStream = fs.createReadStream("hello.txt", "utf8");
-let writeableStream = fs.createWriteStream("some2.txt");
-readableStream.pipe(writeableStream);
+let writeableStream = fs.createWriteStream("hello.txt.gz");
+let gzip = zlib.createGzip();
+readableStream.pipe(gzip).pipe(writeableStream);
