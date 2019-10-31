f6fdad69652026d59b75fc05d00b161673709722
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:06:33 +0400

    ver 0.9

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-function displaySync(data){
-    console.log(data);
+function display(data, callback){ 
+var randInt = Math.random() * (10 - 1) + 1;// с помощью случайного числа определяем ошибку
+var err = randInt>5? new Error("Ошибка выполнения. randInt больше 5"): null;
+setTimeout(function(){
+callback(err, data);
+    }, 0);
 }
 console.log("Начало работы программы");
-displaySync("Обработка данных...");
+display("Обработка данных...", function (err, data){ 
+    if(err) throw err;
+    console.log(data);
+});
 console.log("Завершение работы программы");
