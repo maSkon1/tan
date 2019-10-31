948e41161de739f12a6843bba708a82fb894fc7a
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:25:48 +0400

    ver 0.10.2

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict';
 const fs = require("fs");
-fs.writeFile("hello.txt", "Hello мир!", function(error){
-    if(error) throw error; // если возникла ошибка
-    console.log("Асинхронная запись файла завершена. Содержимое файла:");
+fs.appendFileSync("hello.txt", "Привет ми ми ми!");
+fs.appendFile("hello.txt", "Привет МИД!", function(error){
+    if(error) throw error; // если возникла ошибка                
+    console.log("Запись файла завершена. Содержимое файла:");
     let data = fs.readFileSync("hello.txt", "utf8");
     console.log(data);  // выводим считанные данные
 });
