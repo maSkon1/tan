db009e915ad6d51d60207d4aabc87e10c109f1eb
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:12:07 +0400

    ver 0.9.1

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-function display(data, callback){ 
-var randInt = Math.random() * (10 - 1) + 1;// с помощью случайного числа определяем ошибку
-var err = randInt>5? new Error("Ошибка выполнения. randInt больше 5"): null;
-setTimeout(function(){
-callback(err, data);
-    }, 0);
+function displaySync(callback){
+    callback();
 }
 console.log("Начало работы программы");
-display("Обработка данных...", function (err, data){ 
-    if(err) throw err;
-    console.log(data);
-});
+setTimeout(function(){        
+        console.log("timeout 500");
+}, 500);
+setTimeout(function(){      
+        console.log("timeout 100");
+}, 100);
+displaySync(function(){console.log("without timeout")});
 console.log("Завершение работы программы");
