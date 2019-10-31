2bfe860adf8396991c77f949da8a5e2c037ab46a
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:35:44 +0400

    ver 0.11.1

--- app.js
+++ app.js
@@ -2,10 +2,8 @@
 const Emitter = require("events");
 let emitter = new Emitter();
 let eventName = "greet";
-emitter.on(eventName, function(){
-    console.log("Hello all!");
+emitter.on(eventName, function(data){
+    console.log(data);
 });
-emitter.on(eventName, function(){
-    console.log("Привет!");
-});
-emitter.emit(eventName);
+ 
+emitter.emit(eventName, "Привет пир!");
