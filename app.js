8469ada540badde8f48ddd84c15ab4066b6d5e20
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:48:18 +0400

    ver 0.13

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 'use strict';
 const fs = require("fs");
-let writeableStream = fs.createWriteStream("hello.txt");
-writeableStream.write("Привет мир!");
-writeableStream.write("Продолжение записи \n");
-writeableStream.end("Завершение записи");
 let readableStream = fs.createReadStream("hello.txt", "utf8");
-readableStream.on("data", function(chunk){ 
-    console.log(chunk);
+let writeableStream = fs.createWriteStream("some.txt");
+readableStream.on("data", function(chunk){
+    writeableStream.write(chunk);
 });
