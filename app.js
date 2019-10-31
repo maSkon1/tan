529ca3e7c532e3199f0a6d55aa30c7e10a18d92c
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:23:39 +0400

    ver 0.10.1

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 'use strict';
 const fs = require("fs");
-fs.readFile("hello.txt", "utf8", // асинхронное чтение
-       function(error,data){
-       console.log("Асинхронное чтение файла");
-       if(error) throw error; // если возникла ошибка
-       console.log(data);  // выводим считанные данные
+fs.writeFile("hello.txt", "Hello мир!", function(error){
+    if(error) throw error; // если возникла ошибка
+    console.log("Асинхронная запись файла завершена. Содержимое файла:");
+    let data = fs.readFileSync("hello.txt", "utf8");
+    console.log(data);  // выводим считанные данные
 });
-console.log("Синхронное чтение файла")
-let fileContent = fs.readFileSync("hello.txt", "utf8");// синхронное чтение
-console.log(fileContent);
