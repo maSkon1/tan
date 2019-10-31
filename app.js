c7b42ff1b308f6f45e646ee3b797b431aa80c751
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:01:51 +0400

    ver 0.8

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-'use strict';
-let nodePath = process.argv[0];
-let appPath = process.argv[1];
-let name = process.argv[2];
-let age = process.argv[3];
-console.log("nodePath: " + nodePath);
-console.log("appPath: " + appPath);
-console.log();
-console.log("name: " + name);
-console.log("age: " + age);
+function displaySync(data){
+    console.log(data);
+}
+console.log("Начало работы программы");
+displaySync("Обработка данных...");
+console.log("Завершение работы программы");
