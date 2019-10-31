afda49f8e91429e28e1f7a34a68d18a33b6de4df
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:49:16 +0400

    ver 0.13.1

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 'use strict';
 const fs = require("fs");
 let readableStream = fs.createReadStream("hello.txt", "utf8");
-let writeableStream = fs.createWriteStream("some.txt");
-readableStream.on("data", function(chunk){
-    writeableStream.write(chunk);
-});
+let writeableStream = fs.createWriteStream("some2.txt");
+readableStream.pipe(writeableStream);
