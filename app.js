d9f6421910ea70271ee11ac6eb184d65931908fe
maSkon1 <57221462+maSkon1@users.noreply.github.com>
2019-10-31 16:49:31 +0000

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-http.createServer(function(request,response){ш
-    response.end("Hello NodeJS!");
-}).listen(3030, "127.0.0.1",function(){
-    console.log("Сервер начал прослушивание запросов на порту 3000");
-});
