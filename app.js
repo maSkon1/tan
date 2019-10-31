7ccc35f83279f80593dab1f245e60d509ba07701
maSkon1 <generalowmaks@yandex.ru>
2019-10-31 23:29:58 +0400

    ver 0.10.3

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 'use strict';
-const fs = require("fs");
-fs.appendFileSync("hello.txt", "Привет ми ми ми!");
-fs.appendFile("hello.txt", "Привет МИД!", function(error){
-    if(error) throw error; // если возникла ошибка                
-    console.log("Запись файла завершена. Содержимое файла:");
-    let data = fs.readFileSync("hello.txt", "utf8");
-    console.log(data);  // выводим считанные данные
+fs.unlink("hello.txt", (err) => {
+  if (err) console.log(err); // если возникла ошибка    
+  else console.log("hello.txt was deleted");
 });
