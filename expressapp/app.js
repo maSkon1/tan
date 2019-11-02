00bf81132eec7038e82fdf46f1f3405724c2a9f6
maSkon1 <generalowmaks@yandex.ru>
2019-11-02 18:41:04 +0400

    ver 0.28

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,10 +1,19 @@
 'use strict';
-const express = require("express"); 
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.get("/", function(request, response){   
- console.log(request.query);
-    let id = request.query.user.id;
-    let name = request.query.user.name;   
-    response.send("<h3>id:" + id + "<br>name: " + name +"</h3>");
+// создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = bodyParser.urlencoded({extended: false}); 
+app.get("/register", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/register.html");
 });
+app.post("/register", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+}); 
+app.get("/", function(request, response){
+    response.send("Главная страница");
+});
+  
 app.listen(3130);
