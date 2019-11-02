1831e5e7b7ca63523c31d6e16b7933d32fed2752
maSkon1 <generalowmaks@yandex.ru>
2019-11-02 18:06:32 +0400

    ver 0.27

--- expressapp/app.js
+++ expressapp/app.js
@@ -2,16 +2,9 @@
 const express = require("express"); 
 const app = express();
 app.get("/", function(request, response){   
- response.send("<h1>Главная страница</h1>");
-});
-app.use("/about", function(request, response){    
-    console.log(request.query);
-    let names = request.query.name;
-    let responseText = "<ul>";
-    for(let i=0; i < names.length; i++){
-        responseText += "<li>" + names[i] + "</li>";
-    }
-    responseText += "</ul>";
-    response.send(responseText);
+ console.log(request.query);
+    let id = request.query.user.id;
+    let name = request.query.user.name;   
+    response.send("<h3>id:" + id + "<br>name: " + name +"</h3>");
 });
 app.listen(3130);
