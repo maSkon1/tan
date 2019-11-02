7646b6196c4baba995cdf85ed6efd58996d5723b
maSkon1 <generalowmaks@yandex.ru>
2019-11-02 16:47:44 +0400

    ver 0.23

---
+++ expressapp/app.js
@@ -0,0 +1,11 @@
+// подключение express
+const express = require("express");
+// создаем объект приложения
+const app = express();
+// определяем обработчик для маршрута "/"
+app.get("/", function(request, response){
+    // отправляем ответ
+    response.send("<h2>Привет Express!</h2>");
+});
+// начинаем прослушивать подключения на 3130 порту
+app.listen(3130);
