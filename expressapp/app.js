a2eb3ee0c00e087e017df9f4cdeab3ec8a3cfb6a
maSkon1 <generalowmaks@yandex.ru>
2019-11-03 14:22:10 +0400

    ver 0.35

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const app = express();
-// определяем роутеры
-const userRouter = express.Router();  // для адресов с "/users"
-userRouter.use("/create", function (request, response){
-    response.send("Добавление пользователя");
-});
-userRouter.use("/", function(request, response){
-    response.send("Список пользователей");
-});
-// сопоcтавляем роутер с конечной точкой "/users"
+const userController = require("./controllers/userController.js");
+const homeController = require("./controllers/homeController.js");
+// определяем Router
+const userRouter = express.Router();
+const homeRouter = express.Router();
+// определяем маршруты и их обработчики внутри роутера userRouter
+userRouter.use("/create", userController.addUser);
+userRouter.use("/", userController.getUsers);
 app.use("/users", userRouter);
-// общие обработчики
-app.get("/about", function (request, response) {
-    response.send("О сайте");
-});
-app.get("/", function (request, response) {
-    response.send("Главная страница");
-});
-// обработка ошибки 404
+// определяем маршруты и их обработчики внутри роутера homeRouter
+homeRouter.get("/about", homeController.about);
+homeRouter.get("/", homeController.index);
+app.use("/", homeRouter);
 app.use(function (req, res, next) {
     res.status(404).send("Not Found")
 });
-app.listen(3130);
+ app.listen(3130);
