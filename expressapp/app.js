407b424a9771e07699d7c56c4a63b8c462060e6f
maSkon1 <generalowmaks@yandex.ru>
2019-11-03 15:02:30 +0400

    ver 0.38

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,16 +1,15 @@
+'use strict';
 const express = require("express");
+const multer  = require("multer");
 const app = express();
-const bodyParser = require("body-parser");
-const userRouter = require("./routes/userRouter.js");
-const homeRouter = require("./routes/homeRouter.js");
- 
-app.set("view engine", "hbs");
-app.use(bodyParser.urlencoded({ extended: false }));
- 
-app.use("/users", userRouter);;
-app.use("/", homeRouter);
- 
-app.use(function (req, res, next) {
-    res.status(404).send("Not Found")
-}); 
+app.use(express.static(__dirname));
+app.use(multer({dest:"uploads"}).single("filedata"));
+app.post("/upload", function (req, res, next) {  
+    let filedata = req.file;
+    console.log(filedata);
+    if(!filedata)
+        res.send("Ошибка при загрузке файла");
+    else
+        res.send("Файл загружен");
+});
 app.listen(3130);
