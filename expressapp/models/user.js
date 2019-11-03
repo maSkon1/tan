8b1ab93e3df43e665a6d18d4b22c105ecff48cfc
maSkon1 <generalowmaks@yandex.ru>
2019-11-03 14:52:15 +0400

    ver 0.37

---
+++ expressapp/models/user.js
@@ -0,0 +1,14 @@
+'use strict';
+const users = []; 
+module.exports= class User{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    save(){
+        users.push(this);
+    }
+    static getAll(){
+        return users;
+    }
+}
