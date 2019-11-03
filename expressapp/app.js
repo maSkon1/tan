0e2fb59b6879ee631b86a278ba9295660628b28b
maSkon1 <generalowmaks@yandex.ru>
2019-11-03 13:47:21 +0400

    ver 0.31

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,9 +1,19 @@
-'use strict';
 const express = require("express");
 const app = express();
-app.get("/categories/:categoryId/products/:productId", function (request, response) {
-    let catId = request.params["categoryId"];
-    let prodId = request.params["productId"];
-    response.send(`Категория: ${catId}  Товар: ${prodId}`);
+app.use("/about", function (request, response) {
+  response.send("О сайте");
+});
+app.use("/products/create",function (request, response) {
+  response.send("Добавление товара");
+});
+app.use("/products/:id",function (request, response) {
+  response.send(`Товар ${request.params.id}`);
+});
+app.use("/products/",function (request, response) {
+  response.send("Список товаров");
+});
+ 
+app.use("/", function (request, response) {
+  response.send("Главная страница");
 });
 app.listen(3130);
