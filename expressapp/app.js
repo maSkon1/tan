65e1f904c5578d59fdee7a90b7586d356075800a
maSkon1 <generalowmaks@yandex.ru>
2019-12-02 16:20:27 +0400

    ver 0.43

--- expressapp/app.js
+++ expressapp/app.js
@@ -6,16 +6,11 @@ const connection = mysql.createConnection({
   password: "usbw",
   port: "3307"
 });
-connection.connect(function(err){
-    if (err) {
-      return console.error("Ошибка: " + err.message);
-    }
-    else{
-      console.log("Подключение к серверу MySQL успешно установлено");
-    }
- });
- connection.end(function(err) {
-  if (err) {
-    return console.log("Ошибка: " + err.message);
-  }
-  console.log("Подключение закрыто");
+connection.query("SELECT * FROM users",
+  function(err, results, fields) {
+    console.log(err);
+    console.log(results); // собственно данные
+    console.log(fields); // мета-данные полей 
+});
+connection.end();
+
