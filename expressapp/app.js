411d0a0eb939f0b247c4bfd9eeac66fdc33cc8c8
maSkon1 <generalowmaks@yandex.ru>
2019-12-02 16:42:01 +0400

    ver 0.51

--- expressapp/app.js
+++ expressapp/app.js
@@ -5,11 +5,10 @@ const connection = mysql.createConnection({
   database: "test",
   password: "usbw",
   port: "3307"
-}).promise();
-connection.execute("SELECT * FROM users")
-          .then(([rows, fields]) =>{
-            console.log(rows);
-          })
-          .catch(err =>{
-            console.log(err);
-          });
+});
+connection.query("CREATE DATABASE usersdb2",
+  function(err, results) {
+    if(err) console.log(err);
+    else console.log("База данных создана");
+});
+connection.end();
