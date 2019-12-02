b2652c3d9fd0922ede1d7ef89de3c11ce3eff119
maSkon1 <generalowmaks@yandex.ru>
2019-12-02 17:57:01 +0400

    ver 0.65

--- expressapp/app.js
+++ expressapp/app.js
@@ -1,24 +1,98 @@
-п»їconst mysql = require("mysql2");
+'use strict';
+const mysql = require("mysql2");
+const express = require("express");
+const bodyParser = require("body-parser");
+ 
+const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+ 
 const pool = mysql.createPool({
   connectionLimit: 5,
   host: "localhost",
   user: "root",
-  database: "test",
+  database: "usersdb2",
   password: "usbw",
-  port: "3307"
-}).promise();
-pool.execute("UPDATE users SET age=age+1 WHERE name=?", ["Stan"]) // РёР·РјРµРЅРµРЅРёРµ РѕР±СЉРµРєС‚РѕРІ
-    .then(result =>{ 
-      console.log(result[0]);
-      return pool.execute("SELECT * FROM users"); // РїРѕР»СѓС‡РµРЅРёРµ РѕР±СЉРµРєС‚РѕРІ
-    })
-    .then(result =>{
-      console.log(result[0]);
-      pool.end();
-    })
-    .then(()=>{
-      console.log("РїСѓР» Р·Р°РєСЂС‹С‚");
-    })
-    .catch(function(err) {
-      console.log(err.message);
+  port: "3307",
+});
+ 
+app.set("view engine", "hbs");
+ 
+// получение списка пользователей
+app.get("/", function(req, res){
+    pool.query("SELECT * FROM users", function(err, data) {
+      if(err) return console.log(err);
+      res.render("index.hbs", {
+          users: data
+      });
     });
+});
+// возвращаем форму для добавления данных
+app.get("/create", function(req, res){
+    res.render("create.hbs");
+});
+// получаем отправленные данные и добавляем их в БД 
+app.post("/create", urlencodedParser, function (req, res) {
+         
+    if(!req.body) return res.sendStatus(400);
+    const name = req.body.name;
+    const age = req.body.age;
+    pool.query("INSERT INTO users (name, age) VALUES (?,?)", [name, age], function(err, data) {
+      if(err) return console.log(err);
+      res.redirect("/");
+    });
+});
+ 
+// получем id редактируемого пользователя, получаем его из бд и отправлям с формой редактирования
+app.get("/edit/:id", function(req, res){
+  const id = req.params.id;
+  pool.query("SELECT * FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+     res.render("edit.hbs", {
+        user: data[0]
+    });
+  });
+});
+// получаем отредактированные данные и отправляем их в БД
+app.post("/edit", urlencodedParser, function (req, res) {
+         
+  if(!req.body) return res.sendStatus(400);
+  const name = req.body.name;
+  const age = req.body.age;
+  const id = req.body.id;
+   
+  pool.query("UPDATE users SET name=?, age=? WHERE id=?", [name, age, id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+// получаем id удаляемого пользователя и удаляем его из бд
+app.post("/delete/:id", function(req, res){
+          
+  const id = req.params.id;
+  pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+app.listen(3000, function(){
+  console.log("Сервер ожидает подключения...");
+});
+    res.redirect("/");
+  });
+});
+ 
+// РїРѕР»СѓС‡Р°РµРј id СѓРґР°Р»СЏРµРјРѕРіРѕ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ Рё СѓРґР°Р»СЏРµРј РµРіРѕ РёР· Р±Рґ
+app.post("/delete/:id", function(req, res){
+          
+  const id = req.params.id;
+  pool.query("DELETE FROM users WHERE id=?", [id], function(err, data) {
+    if(err) return console.log(err);
+    res.redirect("/");
+  });
+});
+ 
+app.listen(3000, function(){
+  console.log("РЎРµСЂРІРµСЂ РѕР¶РёРґР°РµС‚ РїРѕРґРєР»СЋС‡РµРЅРёСЏ...");
+});
