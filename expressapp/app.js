fb793c7361aa03b290e517971b3ca681b7ca627b
maSkon1 <generalowmaks@yandex.ru>
2019-12-02 16:53:36 +0400

    ver 0.54

--- expressapp/app.js
+++ expressapp/app.js
@@ -6,8 +6,13 @@ const connection = mysql.createConnection({
   password: "usbw",
   port: "3307"
 });
-const sql = `INSERT INTO users(name, age) VALUES('Sam', 31)`;
-connection.query(sql, function(err, results) {
+const users = [
+  ["Bob", 22],
+  ["Alice", 25],
+  ["Kate", 28]
+];
+const sql = `INSERT INTO users(name, age) VALUES ?`;
+connection.query(sql, [users], function(err, results) {
     if(err) console.log(err);
-    console.log(results.insertId);
+    console.log(results);
 });
