5928abbad4a860db731818246beabcbfa09a613f
maSkon1 <generalowmaks@yandex.ru>
2019-12-02 16:35:43 +0400

    ver 0.49

--- expressapp/app.js
+++ expressapp/app.js
@@ -6,11 +6,12 @@ const connection = mysql.createConnection({
   password: "usbw",
   port: "3307"
 }).promise();
-connection.query("SELECT * FROM users")
-          .then(([rows, fields]) =>{
-            console.log(rows);
-          })
-          .catch(err =>{
-            console.log(err);
-          });
-
+const sql = "INSERT INTO users (name, age) VALUES (?, ?)";
+const user = ["Stan", 19];
+connection.query(sql, user)
+          .then(result =>{
+            console.log(result[0]);
+          })
+          .catch(err =>{
+            console.log(err);
+          });
